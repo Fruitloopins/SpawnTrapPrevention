4c27ade502836c2956c9a22cb16e139e1594a086
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-05-27 14:20:40 +0200

    Protect town spawns

--- src/main/java/net/earthmc/spawntrapprevention/SpawnTrapPrevention.java
+++ src/main/java/net/earthmc/spawntrapprevention/SpawnTrapPrevention.java
@@ -2,8 +2,10 @@ package net.earthmc.spawntrapprevention;
 
 import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent;
-import com.palmergames.bukkit.towny.exceptions.TownyException;
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownBlock;
 import com.palmergames.bukkit.towny.object.WorldCoord;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -12,6 +14,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,16 +29,18 @@ public class SpawnTrapPrevention extends JavaPlugin implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
     public void onPlayerDamagePlayer(TownyPlayerDamagePlayerEvent event) {
-        if (!event.isInWilderness() && isCloseToNationSpawn(event.getLocation()))
+        TownBlock townBlock = event.getTownBlock();
+
+        if (townBlock != null && isCloseToTownySpawn(event.getVictimResident(), townBlock.getWorldCoord()))
             event.setCancelled(true);
     }
 
-    @EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler(priority = EventPriority.LOW)
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (TownyAPI.getInstance().isWilderness(event.getEntity().getLocation()))
             return;
 
-        if (isCloseToNationSpawn(event.getEntity().getLocation())) {
+        if (isCloseToTownySpawn(TownyAPI.getInstance().getResident(event.getEntity()), WorldCoord.parseWorldCoord(event.getEntity().getLocation()))) {
             event.setKeepInventory(true);
             event.setKeepLevel(true);
             event.getDrops().clear();
@@ -42,22 +48,32 @@ public class SpawnTrapPrevention extends JavaPlugin implements Listener {
         }
     }
 
-    private static boolean isCloseToNationSpawn(Location location) {
-        return isCloseToNationSpawn(WorldCoord.parseWorldCoord(location));
-    }
+    private boolean isCloseToTownySpawn(@Nullable Resident resident, @NotNull WorldCoord worldCoord) {
+        final @Nullable Town residentTown = resident != null ? resident.getTownOrNull() : null;
 
-    private static boolean isCloseToNationSpawn(WorldCoord worldCoord) {
         for (WorldCoord coord : selectArea(worldCoord)) {
-            Town town = coord.getTownOrNull();
-            try {
-                if (town != null && town.isCapital() && WorldCoord.parseWorldCoord(town.getNationOrNull().getSpawn()).equals(coord))
+            final Town town = coord.getTownOrNull();
+            if (town == null)
+                continue;
+
+            if (town.equals(residentTown)) {
+                Location spawnLocation = town.getSpawnOrNull();
+                if (spawnLocation != null && WorldCoord.parseWorldCoord(spawnLocation).equals(coord))
                     return true;
-            } catch (TownyException ignored) {}
+            }
+
+            final Nation nation = town.getNationOrNull();
+            if (nation != null && nation.isCapital(town)) {
+                Location spawnLocation = nation.getSpawnOrNull();
+                if (spawnLocation != null && WorldCoord.parseWorldCoord(spawnLocation).equals(coord))
+                    return true;
+            }
         }
+
         return false;
     }
 
-    private static Set<WorldCoord> selectArea(WorldCoord centre) {
+    private Set<WorldCoord> selectArea(WorldCoord centre) {
         Set<WorldCoord> coords = new HashSet<>();
 
         coords.add(centre.add(-1, -1));
